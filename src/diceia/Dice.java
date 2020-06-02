fdc40ecd8ce346cda72bf226571303534e3c342a
Hugo Januario <gr341617@graded.br>
2020-06-02 06:38:37 -0300

    Final Project
    I think this is submitted correctly.
    
    Thanks, Hugo

---
+++ src/diceia/Dice.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diceia;
+
+
+
+/**
+ *
+ * @author hugojanuario
+ */
+public class Dice {
+    private int numSides;
+    private int numDice; //These will be the two main characteristics of the dice that will need editing.
+
+    public Dice(int numOfSides, int numOfDice ){ 
+    this.numSides = numOfSides;
+    this.numDice = numOfDice;
+    }                                //Overloaded constructors for flexibility
+    public Dice( ){
+    this.numSides = 6;
+    this.numDice = 1;
+    }
+    
+
+  
+     
+
+
+    /*
+    loop as many die as you have:   
+     generate a random number between 1 and numSides  out += number; end 
+     loop return output
+    */
+
+  
+    
+    
+    
+} 
+
+
